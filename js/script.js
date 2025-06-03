3ae1855595742dda46144dc22f4df54ad7746533
ALLOMOH <allomohangealex@gmail.com>
2025-06-03 04:16:05 +0000

    loader css

---
+++ js/script.js
@@ -0,0 +1,14 @@
+gsap.fromTo("#loading-page", { opacity: 1 }, { opacity: 0, duration: 1, delay: 2, display: "none" });
+
+gsap.fromTo("#name-logo",
+    {
+        y: 50,
+        opacity: 0,
+    },
+    {
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        delay: 0.5,
+    }
+)
